0fd2b4affae4e6e0e9d7bde3b9f064e87ed6aaf2
antso.tech <hei.antso.2007@gmail.com>
2026-04-28 18:06:50 +0300

    refactor: ameliorate navbar design by adding marigin between components

--- app/components/ui/NavBar.tsx
+++ app/components/ui/NavBar.tsx
@@ -8,51 +8,51 @@ export default function Navbar(){
     return(
         <div className="bg-black text-gray-400 w-60 h-140 pl-1 flex flex-col gap-5">
             <div className="flex flex-row items-center gap-3  pt-5">
-                <button className=" w-10 h-10 rounded-full  bg-[#00E5FF] font-bold text-black">E</button>
-                <h2 className="text-white font-bold text-xl">EventSync</h2>
+                <button className=" w-10 h-10 rounded-full ml-5 bg-[#00E5FF] font-bold text-black">E</button>
+                <h2 className="text-white font-bold text-xl ">EventSync</h2>
             </div>
             <div className="flex flex-col gap-6">
-            <div className="flex flex-col gap-5">
-               <p className="text-xs">EXPLORE</p>
+            <div className="flex flex-col gap-2">
+               <p className="text-xs ml-3">EXPLORE</p>
                 <Link href="/">
-                <span className="flex flex-row items-center gap-2 h-10 p-2 rounded-xl hover:text-black hover:bg-[#00E5FF]">
+                <span className="flex flex-row items-center gap-2 h-10 px-3 w-55 rounded-xl hover:text-black hover:bg-[#00E5FF]">
                     <FontAwesomeIcon icon={faHouse}/>
                     <p>Discover</p>
                 </span>
 
                 </Link>
                 <Link href="/favourites">
-                    <span className="flex flex-row gap-2 items-center hover:text-black hover:bg-[#00E5FF]">
+                    <span className="flex flex-row items-center gap-2 h-10 px-3 w-55 rounded-xl hover:text-black hover:bg-[#00E5FF]">
                        <FontAwesomeIcon icon={faHeart}/>
                        <p>My Favourites</p>
                     </span>
                 </Link>        
                 </div>
-                <div className="flex flex-col gap-4 ">
-                    <span className="flex flex-row gap-2 items-center text-[#00E5FF] hover:text-black hover:bg-[#00E5FF]">
+                <div className="flex flex-col gap-2 ">
+                    <span className="flex flex-row gap-2 items-center text-[#00E5FF] ml-3">
                         <FontAwesomeIcon className="text-xs"icon={faUserShield}/>
                         <h3 className=" text-sm font-semibold">ADMIN</h3>
                     </span>
-                <Link href="/dashboard">
-                    <span className="flex flex-row items-center gap-2 hover:text-black hover:bg-[#00E5FF]">
+                <Link href="admin/dashboard">
+                    <span className="flex flex-row items-center gap-2 h-10 px-3 w-55 rounded-xl hover:text-black hover:bg-[#00E5FF]">
                         <FontAwesomeIcon icon={faTableCells}/>
                         <p>DashBoard</p>
                     </span>
                 </Link>
-                <Link href="/events">
-                    <span className="flex flex-row items-center gap-2 hover:text-black hover:bg-[#00E5FF]">
+                <Link href="admin/events">
+                    <span className="flex flex-row items-center gap-2 h-10 px-3 w-55 rounded-xl hover:text-black hover:bg-[#00E5FF]">
                         <FontAwesomeIcon icon={faCalendar}/>
                         <p>Manage Events</p>
                     </span>
                 </Link>
-                <Link href="/speaker">
-                    <span className="flex flex-row items-center gap-2 hover:text-black hover:bg-[#00E5FF]">
+                <Link href="admin/speakers">
+                    <span className="flex flex-row items-center gap-2 h-10 px-3 w-55 rounded-xl hover:text-black hover:bg-[#00E5FF]">
                         <FontAwesomeIcon icon={faUser}/>
                         <p>Speakers</p>
                     </span>
                 </Link>
-                <Link href="/room">
-                    <span className="flex flex-row items-center gap-2 hover:text-black hover:bg-[#00E5FF]">
+                <Link href="admin/rooms">
+                    <span className="flex flex-row items-center gap-2 h-10 px-3 w-55 rounded-xl hover:text-black hover:bg-[#00E5FF]">
                         <FontAwesomeIcon icon={faDoorOpen}/>
                         <p>Rooms</p>
                     </span>
@@ -60,7 +60,7 @@ export default function Navbar(){
                 </div>
             </div>
             <div>
-                <button>
+                <button className="flex flex-row items-center">
                     <FontAwesomeIcon icon={faArrowRightFromBracket}/>
                     <p>Logout</p>
                 </button>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,32 @@
 import { redirect } from "next/navigation";
 import Navbar from "../app/components/ui/NavBar"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch, faStar } from "@fortawesome/free-solid-svg-icons";
 
 export default function RootPage() {
   return(
-    <div>
+    <div className="flex flex-row">
+      <div>
         <Navbar/>
+      </div>
+    <div>
+    <div>
+      <div className="flex flex-row items-center gap-2 bg-blue-500">
+        <FontAwesomeIcon icon={faStar}/>
+        <p>Connect & sync</p></div>
+          <div>
+            <h1>Synchronize Your </h1>
+            <span>Event Experience</span>
+            <p>Discover the most exciting tech conference, workshops and meetups. </p>
+            <p>Manage your schedule, connect with speakers, and never miss a beat</p>
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faSearch}/>
+            <input type="text" />
+            <button>Search</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
  // redirect("/login");
