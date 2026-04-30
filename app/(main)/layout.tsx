65f658a59ec2f88dd58e0cd6dc9db6c8e066b248
harena <tsutoru24@gmail.com>
2026-04-30 09:42:10 +0300

    feat(home): add footer component

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,4 +1,6 @@
 import Sidebar from "../components/Sidebar";
+import Navbar from "../components/Sidebar";
+import Footer from "../components/Fotter";
 
 export default function MainLayout({
   children,
@@ -6,10 +8,14 @@ export default function MainLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen bg-[#030507] text-white flex">
+    <div className="min-h-screen bg-[#030507] text-white flex flex-col">
       <Sidebar />
-      <div className="ml-[60px] flex-1">
-        {children}
+      <div className="ml-[60px] flex-1 flex flex-col pt-14">
+        <Navbar />
+        <div className="flex-1">
+          {children}
+        </div>
+        <Footer />
       </div>
     </div>
   );
